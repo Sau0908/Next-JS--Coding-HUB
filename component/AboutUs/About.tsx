3df55d74116a89105f0d7e0c5283b2efa3f5778f
Saurabh Kashyap <125493327+Sau0908@users.noreply.github.com>
2023-12-15 15:20:30 +0530

    Update About.tsx

--- component/AboutUs/About.tsx
+++ component/AboutUs/About.tsx
@@ -3,61 +3,8 @@ import React from "react";
 const About = () => {
   return (
     <div id="about">
-      About Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
-      mollitia eos aperiam beatae voluptatum corporis. Excepturi quasi quae
-      ipsam esse voluptatibus voluptates pariatur, praesentium a commodi
-      perspiciatis doloremque tempora qui ex eveniet eligendi non saepe iusto in
-      architecto nostrum voluptatem illum! Adipisci ullam quibusdam laborum.
-      Mollitia fugit minus, ipsam inventore suscipit natus architecto, iste esse
-      ea ipsa sequi repellendus, aliquid sunt recusandae voluptates! Minus error
-      quibusdam velit, consequatur nesciunt id iste? Hic aliquam ex fugiat
-      commodi odit aut labore quis consectetur? Aspernatur adipisci provident
-      voluptates est nihil deleniti maiores amet tempore dignissimos ducimus.
-      Nisi aut, at reiciendis ab tenetur mollitia assumenda esse repellendus
-      aspernatur dolor odio ducimus dignissimos! Suscipit voluptate molestias
-      natus iure dolorem reiciendis autem non, laborum, perferendis ipsam ea a
-      quas, dignissimos aliquam facilis aspernatur harum. Corrupti amet iste
-      assumenda, inventore dolores nostrum iure, repudiandae ducimus recusandae
-      fugit quibusdam repellat fuga facere eaque temporibus veniam qui,
-      voluptatum debitis explicabo nulla expedita voluptate. Quibusdam unde a
-      praesentium excepturi veniam voluptatum suscipit pariatur neque, tenetur
-      saepe sunt doloremque itaque, quidem necessitatibus hic quaerat aliquam
-      eos rem laborum dolorem! Vel sed fuga soluta autem tenetur, voluptatibus
-      rem facere quidem a! Velit repudiandae ducimus illum, id recusandae
-      debitis similique aperiam officiis, quod architecto ex dolore voluptates
-      molestias, quia corporis cum optio? Inventore veritatis vero, iste dolor,
-      fugiat aliquid animi nostrum tempore mollitia reprehenderit maiores soluta
-      explicabo facere! Ullam modi aliquam vel quas quidem quasi pariatur esse
-      atque illo, fugit explicabo! Asperiores quibusdam recusandae nobis harum
-      soluta qui adipisci quidem quis eligendi deleniti vel saepe vero autem
-      sint repellendus fugiat, officia modi reprehenderit dicta consequatur
-      consequuntur culpa blanditiis ipsam non. Exercitationem, accusamus
-      laudantium repudiandae, aliquam, sed in eius rerum ut debitis asperiores
-      nemo sit. Iusto, odit expedita praesentium maiores exercitationem ullam,
-      officiis impedit delectus tempora placeat reprehenderit tempore. Hic,
-      tenetur id ex expedita laudantium eum enim mollitia illum sunt, ullam qui
-      odit nihil reprehenderit recusandae tempore modi doloribus. Non id, sed
-      distinctio minus recusandae quam numquam cupiditate illo ratione facilis
-      doloribus tempora sint. Est maiores, ad at reprehenderit facere nisi
-      perspiciatis, vel quis et unde dolorem! Excepturi possimus neque voluptas
-      veniam dicta, perferendis assumenda optio asperiores iusto quae! Delectus,
-      maiores dolores dolorum, fugit ratione ullam rem modi ducimus voluptas
-      odit perspiciatis impedit laudantium quasi totam similique, eligendi
-      repudiandae cumque nesciunt enim neque quae. Fuga non, commodi deleniti
-      accusamus vero eius quaerat repellat praesentium amet doloremque et eaque
-      magni corrupti, nobis ex. Nihil dolor laudantium debitis hic quisquam
-      nemo. Beatae, maiores. Aut molestiae magnam atque? Ipsam voluptas aperiam
-      dicta eaque! Odio suscipit dolores tempore vel assumenda quibusdam
-      repellat nostrum autem illum rerum omnis, nemo voluptates dolore in facere
-      earum alias. Pariatur hic numquam velit cupiditate aliquid dolorum nam id
-      laborum, nemo at quis? Cupiditate id numquam illo nesciunt corporis maxime
-      commodi nobis velit. Necessitatibus architecto nulla hic, distinctio
-      veritatis cum voluptas eligendi deserunt perferendis nostrum iusto, maxime
-      quo, magni reprehenderit ullam amet nemo. Laboriosam dolore assumenda
-      animi pariatur ipsa obcaecati rem totam vitae fugiat dolorem consequuntur,
-      odit facere fuga doloribus exercitationem corporis unde quo numquam ex et.
-      Beatae voluptatum quos odit asperiores dolores perspiciatis distinctio
-      facilis.
+     About Page
+      
     </div>
   );
 };
